f505e142bbcbbb97b41ad89f0b1a479f45717b60
BLxcwg666 <huixcwg@gmail.com>
2024-02-11 16:13:38 +0800

    [4.3] beta: turnstile on report route

--- routes/public/report.js
+++ routes/public/report.js
@@ -14,25 +14,46 @@ const { webModel } = require('../../modules/sqlModel');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { id, reason } = req.body;
+    const { id, reason, vk } = req.body;
     const ip = req.headers['x-forwarded-for'] || req.ip;
+    const ua = req.headers['user-agent'];
 
-    try {
-        if (!id || !reason || isNaN(id)) { return res.json({ success: false, msg: "有坏蛋，我不说是谁 ╭(╯^╰)╮~" }); }
+    if (vk) {
+        let formData = new FormData();
+        formData.append('secret', process.env.CF_SECRET_KEY);
+        formData.append('response', vk);
+        formData.append('remoteip', ip);
 
-        const web = await webModel.findByPk(id);
+        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+        const result = await fetch(url, {
+            body: formData,
+            method: 'POST',
+        });
 
-        if (!web) {
-            res.json({ success: false, msg: "没有找到指定的站点喵~" });
+        const outcome = await result.json();
+        if (outcome.success) {
+            try {
+                if (!id || !reason || isNaN(id)) { return res.json({ success: false, msg: "有坏蛋，我不说是谁 ╭(╯^╰)╮~" }); }
+
+                const web = await webModel.findByPk(id);
+
+                if (!web) {
+                    res.json({ success: false, msg: "没有找到指定的站点喵~" });
+                } else {
+                    await sendMessage(`<strong>接到新的站点投诉！</strong>\n\n 站点编号：${id} \n 站点名称：${web.name} \n 站点网址：${web.link} \n 投诉理由：${reason}\n\nIP：${ip}\nUser-Agent：${ua}`);
+                    console.log(chalk.cyan(`[${global.time()}] [INFO] Received Report: ${ip} reported ${id}`));
+                    res.json({ success: true, msg: "举报成功，感谢您的贡献（ '▿ ' ）"})
+                }
+
+            } catch (error){
+                console.log(chalk.red(`[${global.time()}] [ERROR]`, error));
+                res.json({ success: false, msg: "出错了呜呜呜~ 请检查控制台输出喵~" });
+            }
         } else {
-            await sendMessage(`<strong>接到新的站点投诉！</strong>\n\n 站点编号：${id} \n 站点名称：${web.name} \n 站点网址：${web.link} \n 投诉理由：${reason}`);
-            console.log(chalk.cyan(`[${global.time()}] [INFO] Received Report: ${ip} reported ${id}`));
-            res.json({ success: true, msg: "举报成功，感谢您的贡献（ '▿ ' ）"})
+            res.json({ success: false, msg: "验证失败"})
         }
-
-    } catch (error){
-        console.log(chalk.red(`[${global.time()}] [ERROR]`, error));
-        res.json({ success: false, msg: "出错了呜呜呜~ 请检查控制台输出喵~" });
+    } else {
+        res.json({ success: false, msg: "验证失败"})
     }
 });
 
