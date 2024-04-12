2e87919e5630236962adc74f426a2971af583a0e
BLxcwg666 <huixcwg@gmail.com>
2024-04-13 03:24:16 +0800

    chore(deps): bump nekologger from 1.0.0 to 1.0.5

--- modules/logger.js
+++ modules/logger.js
@@ -5,36 +5,76 @@
 //  |_| \_|\___|_|\_\___/  |_____\___/ \__, |\__, |\___|_|
 //                                     |___/ |___/
 // A Simple Node.JS Logger based on Chalk Module
+// Version 1.0.5 | https://github.com/NyaStudio/nekologger
 // By BLxcwg666 <huixcwg@gmail.com>
 // Start at 2024/02/22 23:10 CST
 
+const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 const moment = require('moment-timezone');
+const dotenv = require('dotenv').config();
 
 function time() {
     return moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss');
 }
 
+const logPath = process.env.LOG_PATH || path.join(__dirname, './logs');
+if (!fs.existsSync(logPath) && process.env.LOG_ENABLE === 'true') {
+    fs.mkdirSync(logPath);
+}
+
 const logger = {
+    enableWrite: process.env.LOG_ENABLE === 'true',
+    logStream: null,
+
+    initLogStream: function() {
+        if (this.enableWrite) {
+            this.logStream = fs.createWriteStream(path.join(logPath, `${moment().tz('Asia/Shanghai').format('YYYY-MM-DD HH-mm-ss')}.log`), { flags: 'a' });
+        }
+    },
+
     info: function (msg, who) {
-        console.log(chalk.blue(`[${time()}] [${who}] [INFO] ${msg}`))
+        const log = `[${time()}] [${who}] [INFO] ${msg}`;
+        console.log(chalk.blue(log));
+        if (this.enableWrite) {
+            this.logStream.write('\n' + log);
+        }
     },
 
     ok: function (msg, who) {
-        console.log(chalk.green(`[${time()}] [${who}] [OK] ${msg}`))
+        const log = `[${time()}] [${who}] [OK] ${msg}`;
+        console.log(chalk.green(log));
+        if (this.enableWrite) {
+            this.logStream.write('\n' + log);
+        }
     },
 
     warn: function (msg, who) {
-        console.log(chalk.yellow(`[${time()}] [${who}] [WARN] ${msg}`))
+        const log = `[${time()}] [${who}] [WARN] ${msg}`;
+        console.log(chalk.yellow(log));
+        if (this.enableWrite) {
+            this.logStream.write('\n' + log);
+        }
     },
 
     err: function (msg, who) {
-        console.log(chalk.red(`[${time()}] [${who}] [ERROR] ${msg}`))
+        const log = `[${time()}] [${who}] [ERROR] ${msg}`;
+        console.log(chalk.red(log));
+        if (this.enableWrite) {
+            this.logStream.write('\n' + log);
+        }
     },
 
     ing: function (msg, who) {
-        console.log(chalk.cyan(`[${time()}] [${who}] [INFO] ${msg}`))
+        const log = `[${time()}] [${who}] [INFO] ${msg}`;
+        console.log(chalk.cyan(log));
+        if (this.enableWrite) {
+            this.logStream.write('\n' + log);
+        }
     }
-}
+};
+
+logger.initLogStream();
 
 module.exports = logger;
