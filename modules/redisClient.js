c5853213f8cd4cb34fdb47d5f12a9d06724ef704
BLxcwg666 <huixcwg@gmail.com>
2024-04-13 15:18:48 +0800

    fix(init): redis connect url

--- modules/redisClient.js
+++ modules/redisClient.js
@@ -2,8 +2,7 @@ const redis = require('redis');
 const config = require('../config');
 
 const redisClient = redis.createClient({
-    host: config.REDIS_HOST,
-    port: config.REDIS_PORT
+    url: `redis://${config.REDIS_HOST}:${config.REDIS_PORT}`
 });
 
 module.exports = redisClient;
