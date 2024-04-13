7b21e0c56fe2d57a562926c9865566a9d972de38
BLxcwg666 <huixcwg@gmail.com>
2024-04-13 15:19:20 +0800

    fix(config): load dotenv

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 // 别动这个
 
+if (!process.env.LOADED_CONFIG) {
+    require('dotenv').config(); // 加载 .env 文件中的环境变量
+    process.env.LOADED_CONFIG = true; // 标记 dotenv 已加载
+}
+
 const config = {
     // API
     API_PORT: process.env.API_PORT || 3000,
