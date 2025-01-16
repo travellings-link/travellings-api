af27dc9fab1c614b4ea01d6842d8fc35644898e8
BLxcwg666 <huixcwg@gmail.com>
2025-01-16 23:04:58 +0800

    fix(sync): success msg

--- routes/private/syncUpdate.js
+++ routes/private/syncUpdate.js
@@ -24,6 +24,7 @@ const repoPaths = {
 router.post('/:repo', (req, res) => {
   const repoName = req.params.repo;
   const repoPath = repoPaths[repoName];
+  const ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.ip;
 
   if (!repoPath) {
     return res.json({ success: false, msg: "至少要告诉我同步哪个仓库吧 (-`ェ´-╬)" });
@@ -40,7 +41,7 @@ router.post('/:repo', (req, res) => {
       res.status(500).json({ success: false, msg: "出错了呜呜呜~ 请检查控制台输出喵~" });
     }
 
-    log.ok(`Failed to pull：${stderr}`, "SYNC")
+    log.ok(`Sync successfully from ${ip}`, "SYNC")
     res.json({ success: true, msg: "同步成功 (*´▽`*)" });
   });
 });
